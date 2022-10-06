a0b1c5b82d0453cb97395c34ad469f09f98d509d
Zoé <100842308+ZoePlayoust@users.noreply.github.com>
2022-10-06 10:03:06 +0200

    Finishing setting up files
    Added all the different components and related stylesheets.

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import '../Playlist/Playlist.css';
+import { Tracklist } from '../Tracklist/Tracklist';
+
+
+export class Playlist extends React.Component{
+render (){
+return(
+<div className="Playlist">
+  <input value="New Playlist"/>
+    <Tracklist />
+  <button className="Playlist-save">SAVE TO SPOTIFY</button>
+</div>
+)
+    }
+}
+
+Playlist.defaultProps ={
+    value: "New Playlist"
+    
+}
